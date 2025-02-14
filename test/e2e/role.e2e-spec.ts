85d049516efe08545a0b4793e45def80a160cf94
Agus3160 <ampharos321@hotmail.com>
2025-02-14 18:08:34 -0300

    [143] Modificacion de mock findAll para verificar response correctamente

--- test/e2e/role.e2e-spec.ts
+++ test/e2e/role.e2e-spec.ts
@@ -5,7 +5,6 @@ import { RoleService } from '@/role/role.service';
 import { RoleController } from '@/role/role.controller';
 import { RoleDto } from '@/role/dto/role.dto';
 
-// Datos de mock y respuesta
 const roleMock = {
 	id: 1,
 	name: 'ADMIN',
@@ -24,24 +23,24 @@ const paginatedResultMock = {
 	prev: false,
 };
 
-// Servicio mockeado
 const roleService = {
 	update: jest.fn().mockResolvedValue(roleMock),
 	remove: jest.fn().mockResolvedValue({ id: 1, deletedAt: new Date() }),
 	create: jest.fn().mockResolvedValue(roleMock),
-	findAll: jest.fn().mockResolvedValue(paginatedResultMock),
+	findAll: jest.fn().mockImplementation(({ page: currentPage, size }) => ({
+		...paginatedResultMock,
+		currentPage,
+		size,
+	})),
 };
 
 describe('RoleController (e2e)', () => {
 	let app: INestApplication;
-
-	// Centralizar la configuración del servicio y aplicación
 	const setupApp = async () => {
 		const moduleRef = await Test.createTestingModule({
 			controllers: [RoleController],
 			providers: [{ provide: RoleService, useValue: roleService }],
 		}).compile();
-
 		app = moduleRef.createNestApplication();
 		app.useGlobalPipes(new ValidationPipe({ transform: true }));
 		await app.init();
@@ -50,12 +49,10 @@ describe('RoleController (e2e)', () => {
 	beforeAll(setupApp);
 	afterAll(async () => await app.close());
 
-	// Reducción de código repetido para realizar las verificaciones comunes
 	const assertResponse = (response, expectedBody) => {
 		expect(response.body).toMatchObject<RoleDto>(expectedBody);
 	};
 
-	// Reducción de pruebas repetitivas
 	it('/GET role [error in query parse]', async () => {
 		const url = '/role?page=-1';
 		await request(app.getHttpServer()).get(url).expect(400);
@@ -73,11 +70,13 @@ describe('RoleController (e2e)', () => {
 		});
 
 		assertResponse(response, {
-			id: expect.any(Number),
-			name: 'ADMIN',
-			createdAt: expect.any(String),
-			updatedAt: expect.any(String),
-			deletedAt: null,
+			data: expect.any(Array),
+			currentPage: 1,
+			size: 20,
+			totalPages: expect.any(Number),
+			total: expect.any(Number),
+			next: expect.any(Boolean),
+			prev: expect.any(Boolean),
 		});
 	});
 
