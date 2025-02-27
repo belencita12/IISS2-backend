07aa9d267221fbad39645e13bb575b16cde5473e
Luciana <lucianascheid@fiuni.edu.py>
2025-02-26 22:18:59 -0300

    [175] guard del auth

--- src/auth/guard/auth.guard.ts
+++ src/auth/guard/auth.guard.ts
@@ -9,6 +9,8 @@ import { Request } from 'express';
 import { TokenPayload } from '../types/auth.types';
 import { EnvService } from '@/env/env.service';
 import { UserService } from '@/user/user.service';
+import { Reflector } from '@nestjs/core';
+import { IS_PUBLIC_KEY } from '@/lib/decorators/public.decorator';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
@@ -16,15 +18,28 @@ export class AuthGuard implements CanActivate {
 		private jwtService: JwtService,
 		private userService: UserService,
 		private env: EnvService,
+		private reflector: Reflector,
 	) {}
 
 	async canActivate(context: ExecutionContext): Promise<boolean> {
+		
+		const isPublic = this.reflector.getAllAndOverride<boolean>(IS_PUBLIC_KEY,[
+			context.getHandler(),
+			context.getClass(),
+		])
+	
+		if(isPublic){
+			return true;
+		}
+
 		const request: Request = context.switchToHttp().getRequest();
 		const token = this.extractTokenFromHeader(request);
 		const secret = this.env.get('JWT_SECRET');
+
 		if (!token) {
 			throw new UnauthorizedException();
 		}
+		
 		try {
 			const payload: TokenPayload = await this.jwtService.verifyAsync(token, {
 				secret,
