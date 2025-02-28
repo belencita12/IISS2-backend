c9eb9018feb3e97867931ec83e3d56ea334ef03b
Agus3160 <ampharos321@hotmail.com>
2025-02-28 09:49:32 -0300

    Se anhade el id del usuario en el response de signin

--- src/auth/dto/sign-in-res.dto.ts
+++ src/auth/dto/sign-in-res.dto.ts
@@ -1,7 +1,11 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsString } from 'class-validator';
+import { IsInt, IsString } from 'class-validator';
 
 export class SignInResponseDto {
+	@ApiProperty()
+	@IsInt()
+	id: number;
+
 	@ApiProperty()
 	@IsString()
 	username: string;
