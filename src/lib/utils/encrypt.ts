30212986d688427317429ca787599c4617495c8e
Agus3160 <ampharos321@hotmail.com>
2025-02-28 21:42:45 -0300

    Migracion creada, uso de uuid por mayor compatibilidad con tests y refactor de tests para adaptarse al nuevo campo

--- src/lib/utils/encrypt.ts
+++ src/lib/utils/encrypt.ts
@@ -1,5 +1,5 @@
 import * as bcrypt from 'bcrypt';
-import { customAlphabet } from 'nanoid';
+import { v4 as uuidv4 } from 'uuid';
 
 export const hash = async (str: string) => {
 	const hash = await bcrypt.hash(str, 10);
@@ -10,12 +10,6 @@ export const compare = async (str: string, hash: string) => {
 	return await bcrypt.compare(str, hash);
 };
 
-const alphabet =
-	process.env.NANOID_ALPHABET ||
-	'0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ_abcdefghijklmnopqrstuvwxyz';
-const length = Number(process.env.NANOID_LENGTH) || 16;
-
 export const genUsername = (fullName: string) => {
-	const nanoid = customAlphabet(alphabet, length);
-	return fullName.split(' ').join('_').toLowerCase() + '@' + nanoid();
+	return fullName.split(' ').join('_').toLowerCase() + '@' + uuidv4();
 };
