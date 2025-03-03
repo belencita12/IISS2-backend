70bf4530e77b6cddea8465616e315a74f2405298
Agus3160 <ampharos321@hotmail.com>
2025-03-03 09:19:10 -0300

    Cambio de permisos sobre species y race

--- src/species/species.controller.ts
+++ src/species/species.controller.ts
@@ -1,4 +1,14 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, Query, UseGuards } from '@nestjs/common';
+import {
+	Controller,
+	Get,
+	Post,
+	Body,
+	Patch,
+	Param,
+	Delete,
+	Query,
+	UseGuards,
+} from '@nestjs/common';
 import { SpeciesService } from './species.service';
 import { CreateSpeciesDto } from './dto/create-species.dto';
 import { UpdateSpeciesDto } from './dto/update-species.dto';
@@ -10,42 +20,52 @@ import { RolesGuard } from '@/lib/guard/role.guard';
 import { Roles } from '@/lib/decorators/roles.decorators';
 import { Role } from '@/lib/constants/role.enum';
 
-
-@UseGuards(RolesGuard)
-@Roles(Role.Admin)
 @Controller('species')
 @ApiTags('Species')
 export class SpeciesController {
-  constructor(private readonly speciesService: SpeciesService) {}
+	constructor(private readonly speciesService: SpeciesService) {}
 
-  @Post()
-  @ApiResponse({type: SpeciesDto})
-  @ApiBody({type: CreateSpeciesDto})
-  async create(@Body() createSpeciesDto: CreateSpeciesDto) {
-    return this.speciesService.create(createSpeciesDto);
-  }
+	@UseGuards(RolesGuard)
+	@Roles(Role.Admin)
+	@Post()
+	@ApiResponse({ type: SpeciesDto })
+	@ApiBody({ type: CreateSpeciesDto })
+	async create(@Body() createSpeciesDto: CreateSpeciesDto) {
+		return this.speciesService.create(createSpeciesDto);
+	}
 
-  @Get()
-  @ApiPaginatedResponse(SpeciesDto)
-  async findAll(@Query() query: SpeciesQueryDto) {
-    return this.speciesService.findAll(query);
-  }
+	@UseGuards(RolesGuard)
+	@Roles(Role.Admin, Role.User)
+	@Get()
+	@ApiPaginatedResponse(SpeciesDto)
+	async findAll(@Query() query: SpeciesQueryDto) {
+		return this.speciesService.findAll(query);
+	}
 
-  @Get(':id')
-  @ApiResponse({type: SpeciesDto})
-  async findOne(@Param('id') id: string) {
-    return this.speciesService.findOne(+id);
-  }
+	@UseGuards(RolesGuard)
+	@Roles(Role.Admin, Role.User)
+	@Get(':id')
+	@ApiResponse({ type: SpeciesDto })
+	async findOne(@Param('id') id: string) {
+		return this.speciesService.findOne(+id);
+	}
 
-  @Patch(':id')
-  @ApiResponse({ type: SpeciesDto })
-  async update(@Param('id') id: string, @Body() updateSpeciesDto: UpdateSpeciesDto) {
-    return this.speciesService.update(+id, updateSpeciesDto);
-  }
+	@UseGuards(RolesGuard)
+	@Roles(Role.Admin)
+	@Patch(':id')
+	@ApiResponse({ type: SpeciesDto })
+	async update(
+		@Param('id') id: string,
+		@Body() updateSpeciesDto: UpdateSpeciesDto,
+	) {
+		return this.speciesService.update(+id, updateSpeciesDto);
+	}
 
-  @Delete(':id')
-  @ApiResponse({ type: SpeciesDto })
-  async remove(@Param('id') id: string) {
-    return this.speciesService.remove(+id);
-  }
+	@UseGuards(RolesGuard)
+	@Roles(Role.Admin)
+	@Delete(':id')
+	@ApiResponse({ type: SpeciesDto })
+	async remove(@Param('id') id: string) {
+		return this.speciesService.remove(+id);
+	}
 }
