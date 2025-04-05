9179eaf0892f72453ddb5a5928750b1ba8448555
Agus3160 <ampharos321@hotmail.com>
2025-04-05 11:26:25 -0300

    [318] Uso de soft delete en crud proveedores

--- src/features/provider/provider.service.ts
+++ src/features/provider/provider.service.ts
@@ -8,67 +8,69 @@ import { ProviderDto } from './dto/provider.dto';
 
 @Injectable()
 export class ProviderService {
-    constructor(private prisma: PrismaService) {}
+	constructor(private prisma: PrismaService) {}
 
-    async create(dto: CreateProviderDto) {
-        const provider = await this.prisma.provider.create({ data: dto });
-        return new ProviderDto(provider);
-    }
+	async create(dto: CreateProviderDto) {
+		const provider = await this.prisma.provider.create({ data: dto });
+		return new ProviderDto(provider);
+	}
 
-    async findOne(id: number) {
-        const provider = await this.prisma.provider.findUnique({ where: { id } });
-        if (!provider) throw new NotFoundException('Proveedor no encontrado');
-        return new ProviderDto(provider);
-    }
+	async findOne(id: number) {
+		const provider = await this.prisma.provider.findUnique({ where: { id } });
+		if (!provider) throw new NotFoundException('Proveedor no encontrado');
+		return new ProviderDto(provider);
+	}
 
-    async update(id: number, dto: UpdateProviderDto) {
-        const exists = await this.prisma.provider.isExists({ id });
-        if (!exists) throw new NotFoundException('Proveedor no encontrado');
+	async update(id: number, dto: UpdateProviderDto) {
+		const exists = await this.prisma.provider.isExists({ id });
+		if (!exists) throw new NotFoundException('Proveedor no encontrado');
 
-        const updated = await this.prisma.provider.update({ where: { id }, data: dto });
-        return new ProviderDto(updated);
-    }
+		const updated = await this.prisma.provider.update({
+			where: { id },
+			data: dto,
+		});
+		return new ProviderDto(updated);
+	}
 
-    async remove(id: number) {
-        const exists = await this.prisma.provider.isExists({ id });
-        if (!exists) throw new NotFoundException('Proveedor no encontrado');
-        return await this.prisma.provider.delete({ where: { id } });
-    }
+	async remove(id: number) {
+		const exists = await this.prisma.provider.isExists({ id });
+		if (!exists) throw new NotFoundException('Proveedor no encontrado');
+		return await this.prisma.provider.softDelete({ id });
+	}
 
-    async findAll(dto: ProviderQueryDto) {
-        const { baseWhere } = this.prisma.getBaseWhere(dto);
-        const where: Prisma.ProviderWhereInput = {
-            ...baseWhere,
-            ...(dto.query ? this.getWhereByQuerySearch(dto.query) : {}),
-        };
+	async findAll(dto: ProviderQueryDto) {
+		const { baseWhere } = this.prisma.getBaseWhere(dto);
+		const where: Prisma.ProviderWhereInput = {
+			...baseWhere,
+			...(dto.query ? this.getWhereByQuerySearch(dto.query) : {}),
+		};
 
-        const [providers, total] = await Promise.all([
-            this.prisma.provider.findMany({ ...this.prisma.paginate(dto), where }),
-            this.prisma.provider.count({ where }),
-        ]);
+		const [providers, total] = await Promise.all([
+			this.prisma.provider.findMany({ ...this.prisma.paginate(dto), where }),
+			this.prisma.provider.count({ where }),
+		]);
 
-        return this.prisma.getPagOutput({
-            page: dto.page,
-            size: dto.size,
-            total,
-            data: providers.map((provider) => new ProviderDto(provider)),
-        });
-    }
+		return this.prisma.getPagOutput({
+			page: dto.page,
+			size: dto.size,
+			total,
+			data: providers.map((provider) => new ProviderDto(provider)),
+		});
+	}
 
-    private getWhereByQuerySearch(query?: string): Prisma.ProviderWhereInput {
-        let querySearchWhere: Prisma.ProviderWhereInput = {};
-        if (!query?.trim()) return querySearchWhere;
+	private getWhereByQuerySearch(query?: string): Prisma.ProviderWhereInput {
+		let querySearchWhere: Prisma.ProviderWhereInput = {};
+		if (!query?.trim()) return querySearchWhere;
 
-        const searchQuery = query.trim();
-        querySearchWhere = {
-            OR: [
-                { ruc: { contains: searchQuery, mode: 'insensitive' } },
-                { businessName: { contains: searchQuery, mode: 'insensitive' } },
-                { phoneNumber: { contains: searchQuery } }
-            ]
-        };
+		const searchQuery = query.trim();
+		querySearchWhere = {
+			OR: [
+				{ ruc: { contains: searchQuery, mode: 'insensitive' } },
+				{ businessName: { contains: searchQuery, mode: 'insensitive' } },
+				{ phoneNumber: { contains: searchQuery } },
+			],
+		};
 
-        return querySearchWhere;
-    }
+		return querySearchWhere;
+	}
 }
-
