434b1408a14935c76a5cdf4a64987c536c4f1695
Agus3160 <ampharos321@hotmail.com>
2025-04-15 00:05:10 -0300

    [335] Solved DTO name + validation in duration + tags validation

--- src/features/service-type/dto/service-type.dto.ts
+++ src/features/service-type/dto/service-type.dto.ts
@@ -38,6 +38,7 @@ export class ServiceTypeDto {
 
 	constructor(data: ServiceTypeEntity) {
 		this.id = data.id;
+		this.name = data.name;
 		this.slug = data.slug;
 		this.description = data.description;
 		this.durationMin = data.durationMin;
