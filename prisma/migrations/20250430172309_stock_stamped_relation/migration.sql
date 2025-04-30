e90cab98029f18d87dada80495bef483372e7bee
Agus3160 <ampharos321@hotmail.com>
2025-04-30 19:25:57 -0300

    [407] Prev migration del

--- prisma/migrations/20250430172309_stock_stamped_relation/migration.sql
+++ prisma/migrations/20250430172309_stock_stamped_relation/migration.sql
@@ -2,7 +2,6 @@
   Warnings:
 
   - A unique constraint covering the columns `[stampedId]` on the table `Stock` will be added. If there are existing duplicate values, this will fail.
-  - A unique constraint covering the columns `[phoneNumber]` on the table `User` will be added. If there are existing duplicate values, this will fail.
   - Added the required column `stampedId` to the `Stock` table without a default value. This is not possible if the table is not empty.
 
 */
@@ -12,9 +11,6 @@ ADD COLUMN     "stockNum" SERIAL NOT NULL;
 
 -- CreateIndex
 CREATE UNIQUE INDEX "Stock_stampedId_key" ON "Stock"("stampedId");
-
--- CreateIndex
-CREATE UNIQUE INDEX "User_phoneNumber_key" ON "User"("phoneNumber");
 
 -- AddForeignKey
 ALTER TABLE "Stock" ADD CONSTRAINT "Stock_stampedId_fkey" FOREIGN KEY ("stampedId") REFERENCES "Stamped"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
