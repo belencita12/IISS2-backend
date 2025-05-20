562fb6d70554af2b79cb314fe0f01b8672896d3d
Luciana <lucianascheid@fiuni.edu.py>
2025-05-19 23:16:05 -0300

    [442] eliminacion de imports innecesarios

--- src/features/invoice-module/dto/invoice-report-query.dto.ts
+++ src/features/invoice-module/dto/invoice-report-query.dto.ts
@@ -1,8 +1,5 @@
 import { IsDbDate } from '@lib/decorators/validation/is-db-date.decorator';
-import { QueryParam } from '@lib/decorators/validation/query-param.decorator';
 import { ApiProperty } from '@nestjs/swagger';
-import { Type } from 'class-transformer';
-import { IsString } from 'class-validator';
 
 export class InvoiceReportQueryDto {
 	@IsDbDate()
